ae7939e73100cfbb0f1ab14ea7602d556309d827
tonypirchiner-code <tpirchinerprogramacao@gmail.com>
2024-10-20 12:49:05 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function searchZip() {
+        const zipCode = document.getElementById('zipCode').value;
+        alert('Searching for properties in Zip Code: ' + zipCode);
+        // You could add more functionality here, like redirecting to a search results page.
+    }
